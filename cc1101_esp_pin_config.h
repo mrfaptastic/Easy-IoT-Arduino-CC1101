074ce7ebb90c1a96556180e1b97dc347515b91b4
mrfaptastic <gmail@qwe23.com>
2019-07-15 01:02:29 +0100

    Revert "Change interrupt pin on ESP8266 to D3"
    This reverts commit bb3218ed9648e808cfdfab917d22f63b1b660455.

--- cc1101_esp_pin_config.h
+++ cc1101_esp_pin_config.h
@@ -8,9 +8,7 @@
 #if defined ESP32
   #define CC1101_GDO0 34   // Use GPIO PIN 34 as the interrupt pin
 #elif defined ESP8266 // assume it's an ESP8266
-  #define CC1101_GDO0 D3   // Use GPIO PIN D3 as the interrupt pin,
-						   // This lets D1 and D2 be available for I2C (refer to: https://steve.fi/Hardware/d1-pins/)
-						   // All of the IO pins have interrupt/pwm/I2C/one-wire support except D0. (refer to: https://wiki.wemos.cc/products:d1:d1_mini)						   
+  #define CC1101_GDO0 D2   // Use GPIO PIN D2 as the interrupt pin
 #else
   #pragma message "UNKNOWN DEVICE!"
   #define CC1101_GDO0 0
